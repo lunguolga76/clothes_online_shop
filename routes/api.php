b0a8c6987992b18ba099e357c9fce700427f93a8
Olga LUNGU <lunguolga2000@yahoo.com>
2021-10-12 18:04:15 +0300

    add first not working implementation of api endpoint for product reading

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmailCheckApiController;
@@ -18,3 +19,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/email-check', ['uses' => EmailCheckApiController::class]);
+
+Route::group(['prefix' => '/api'], function () {
+    Route::group(['prefix' => '/products'], function () {
+        Route::get('/{id}', ['uses' => ProductsController::class . '@readProduct']);
+    });
+//        Route::get('/info', ['uses' => UserInfoController::class . '@index']);
+//    Route::post('/info', ['uses' => UserInfoController::class . '@storedInfo']);
+//    Route::get('/info/stored', ['uses' => UserInfoController::class . '@stored']);
+
+});
+
+
+//https://localhost:8080/api/products/8523
