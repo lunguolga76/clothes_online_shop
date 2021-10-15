e997a40c133e30c979a67e53ccd95bd0917d636a
Olga LUNGU <lunguolga2000@yahoo.com>
2021-10-15 21:45:17 +0300

    new

--- routes/web.php
+++ routes/web.php
@@ -9,46 +9,45 @@ use App\Http\Controllers\ManufacturerController;
 {
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
- 
-Route::get('/', function() {
-    return view('front.homepage');
-});
-Route::get('/category', function() {
-    return view('front.category');
-});
-Route::get('/category/product/{id}', function() {
-    return view('front.product-details');
-});
-Route::get('/blog', [BlogController::class, 'index'])->name('bloghome');
-
-Route::get('/blog/article/{id}', [BlogController::class, 'show'])->name('article.show');
-
-Route::get('/contacts',[ContactUsController::class, 'contactUs'])->name('contactUs.show');
-
-Route::post('/store-contact-info',[ContactUsController::class, 'storeContactInfo'])->name('contactUs.store');
-Route::get('/manufacturer', [\App\Http\Controllers\ManufacturerController::class, 'create'])->name('manufacturer.store');
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
 
 
-/*})->middleware('response.log');
+    Route::get('/', function() {
+        return view('front.homepage');
+    });
+    Route::get('/category', function() {
+        return view('front.category');
+    });
+    Route::get('/category/product/{id}', function() {
+        return view('front.product-details');
+    });
+    Route::get('/blog', [BlogController::class, 'index'])->name('bloghome');
 
-Route::group(['prefix' => '/user'], function () {
-    Route::get('/info', ['uses' => UserInfoController::class . '@index']);
-    Route::post('/info', ['uses' => UserInfoController::class . '@storedInfo']);
-    Route::get('/info/stored', ['uses' => UserInfoController::class . '@stored']);
+    Route::get('/blog/article/{id}', [BlogController::class, 'show'])->name('article.show');
 
-});
-*/
+    Route::get('/contacts',[ContactUsController::class, 'contactUs'])->name('contactUs.show');
+
+    Route::post('/store-contact-info',[ContactUsController::class, 'storeContactInfo'])->name('contactUs.store');
+    Route::get('/manufacturer', [\App\Http\Controllers\ManufacturerController::class, 'create'])->name('manufacturer.store');
+
+
+    /*})->middleware('response.log');
+    Route::group(['prefix' => '/user'], function () {
+        Route::get('/info', ['uses' => UserInfoController::class . '@index']);
+        Route::post('/info', ['uses' => UserInfoController::class . '@storedInfo']);
+        Route::get('/info/stored', ['uses' => UserInfoController::class . '@stored']);
+    });
+    */
 
 
 }
+
